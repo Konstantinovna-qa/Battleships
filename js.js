774175ce19362b64f6c0d3c4c098e5c3b7d5cd05
Evelina <politko96@inbox.ru>
2023-02-08 19:32:15 +0300

    Исправлен стиль кода

--- js.js
+++ js.js
@@ -23,36 +23,39 @@
  ВЫВЕСТИ статистику
  */  
 
- var location1 = 3; 
- var location2 = 4;
- var location3 = 5;
+ const location1 = 3; 
+ const location2 = 4;
+ const location3 = 5;
 
- var guess; // undefined
- var hits = 0;
- var guesses = 0;
+ let guess; // undefined
+ let hits = 0;
+ let guesses = 0;
 
- var isSunk = false;
+ let isSunk = false;
 
- while(isSunk == false){
+ while (isSunk === false) {
       guess = prompt("Ты готов выстрелить? (введи цифру 0-6):");
-      if(guess < 0 || guess > 6){
+
+      if (guess < 0 || guess > 6) {
            alert("Я же просил от 0 до 6 ввести!"); 
-      }else{
+      } else {
            guesses = guesses + 1;
-           if(guess == location1 || guess == location2 || guess == location3){
+
+           if (guess === location1 || guess === location2 || guess === location3) {
                hits = hits + 1;
-               if(hits == 3){
+
+               if(hits === 3) {
                    isSunk = true;
                    alert("Ты победил!");
                }
-           }else{
+           } else {
                 alert("МИМО)))");
            }
      }
  }
- var status = "Вы выстрелили " + guesses + "Для того чтобы попасть по кораблю," +
- "это показывает уровень вашей точности,как: " + (3/guesses);
- alert("status");
+
+ const status = `Вы выстрелили ${guesses} Для того чтобы попасть по кораблю, это показывает уровень вашей точности, как: ${3 / guesses}`;
+ alert(status);
  /*
  Булевские операторы используются в логических выражениях,результатом которых
  является значение true или false. Булевские операторы деляться на два вида: 
